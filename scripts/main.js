c311034cb5ae8dbeb4decd8e1e0c11d064f6cfa5
vannhan2812 <55873308+vannhan2812@users.noreply.github.com>
2020-12-16 08:22:58 +0700

    sencond comition

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,14 @@
 const myHeading = document.querySelector('h2')
-myHeading.textContent='Hello World'
+myHeading.textContent='Home - Hello , World of Pleasure'
+var button = document.getElementById('clickBtn' )
+var count = 0
+var button = document.getElementById("clickBtn"),
+  count = 0
+button.onclick = function() {
+  count += 1
+  button.innerHTML = "You have jerked " + count + " times so far "
+  if(count%10==0){
+      alert(" i cumed !!!!")
+  }
+}
+
